3d3481409ec73ceea37c19b09dc4c3a38c8b541a
WeilonYing <weilony@hotmail.com>
2018-10-04 15:50:48 +1000

    Begin integration with DataServices

--- src/DataService.ts
+++ src/DataService.ts
@@ -10,19 +10,23 @@ export class DataService {
     return "Angela";
     // TODO: implement nickname creation and retrieval
   }
-  
+
   // Messages are simply stored in a hashmap for now, for testing purposes only
   messages: { [uuid: string]: Payload[] } = {};
-  
+
   public getMessages(uuid: string): Payload[] {
     if (!this.messages[uuid]) {
       return [];
     }
     return this.messages[uuid];
-    
+
     // TODO: implement message retrieval from the database
   }
-  
+  /* From and to represent the message interval. E.g. if from is 0, start from most recent message */
+  public getMessages(uuid: string, from: number, to: number): Payload[] {
+    return null;
+  }
+
   /* Store message in chat history associated with the provided UUID */
   public storeMessage(uuid: string, payload: Payload): void {
     if (!this.messages[uuid]) {
@@ -30,6 +34,6 @@ export class DataService {
     }
     this.messages[uuid].push(payload);
   }
-  
+
   // TODO: implement message storage to the database
 }
