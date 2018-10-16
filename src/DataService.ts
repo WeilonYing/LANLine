e47dae27ce6582a2651f71712454924b1f4abb18
Angela Su <angelasu97@hotmail.com>
2018-10-16 23:36:27 +1100

    [MM05] Reset nickname to Potato Salad.

--- src/DataService.ts
+++ src/DataService.ts
@@ -1,50 +1,44 @@
 const sqlite3 = require('sqlite3').verbose();
 import { Database } from 'sqlite3';
-import { Payload, PayloadUtils } from './Payload';
+import {Payload, PayloadJSON, PayloadUtils} from './Payload';
 import { User } from './User';
+import { Settings } from './Settings';
 
 export class DataService {
-	db: Database;
+  private db: Database;
 
-	constructor() {
-		this.db = new sqlite3.Database('db.sqlite3');
+  constructor() {
+    this.db = new sqlite3.Database("db.sqlite3");
 
-    let messageTableSql = 'CREATE TABLE IF NOT EXISTS messages( \
-                      sender int,                               \
-                      receiver int,                             \
-                      isBroadcast boolean,                      \
-                      nickname string,                          \
-                      timestamp datetime,                       \
-                      message string)'
-    let userTableSql = 'CREATE TABLE IF NOT EXISTS users(       \
-                      UUID int,                                 \
-                      nickname string)'
+    const messageTableSql = "CREATE TABLE IF NOT EXISTS messages( \
+                    sender int,\
+                    receiver int,\
+                    isBroadcast boolean,\
+                    nickname string,\
+                    timestamp datetime,\
+                    message string)";
 
-		this.db.run(messageTableSql);
-		this.db.run(userTableSql);
+    const userTableSql = "CREATE TABLE IF NOT EXISTS users(\
+                    uuid string,\
+                    ip string,\
+                    lastHeartbeat datetime,\
+                    nickname string,\
+                    isOnline boolean,\
+                    isBlocked boolean)";
 
-	}
+    this.db.run(messageTableSql);
+    this.db.run(userTableSql);
+
+  }
 
   public getId(): string {
-    return "angela";
+    return "whale";
     // TODO: implement ID storage and generator
   }
 
   public getNickname(): string {
-    return "Angela";
+    return "Potato Salad";
     // TODO: implement nickname creation and retrieval
-  }
-
-  public getBlockedUsers(): User[]  {
-    let user1 :User = {
-      uuid: "teresa",
-      nickname: "teresa",
-      ip: "10.1.1",
-      lastHeard: new Date(),
-      blockedList: []
-    };
-
-    return [user1];
   }
 
   public getBroadcasts(from?: number, to?: number): Promise<Payload[]> {
@@ -52,22 +46,21 @@ export class DataService {
       from = 0;
       to = 10;
     }
-
-  	let sql = 'SELECT * FROM messages\
+    const sql = "SELECT * FROM messages\
                 WHERE ISBROADCAST = true\
                 ORDER BY timestamp ASC\
                 LIMIT ?\
-                OFFSET ?';
+                OFFSET ?";
 
     return new Promise((resolve, reject) => {
-      const messages : Payload[] = [];
-      this.db.each(sql, [to-from, from], (err, row) => {
+      const messages: Payload[] = [];
+      this.db.each(sql, [to - from, from], (err, row) => {
         if (err) {
           reject("sql failed: " + sql);
         } else {
           messages.push(PayloadUtils.jsonToPayload(
-            {uuid: row.UUID,
-            type: 'broadcast',
+            {uuid: row.uuid,
+            type: "broadcast",
             timestamp: row.timestamp,
             nickname: row.nickname,
             message: row.message}));
@@ -78,7 +71,7 @@ export class DataService {
         } else {
           resolve (messages);
         }
-      })
+      });
     });
   }
 
@@ -93,22 +86,21 @@ export class DataService {
       from = 0;
       to = 10;
     }
-
-  	let sql = 'SELECT * FROM messages\
+    const sql = "SELECT * FROM messages\
                 WHERE (SENDER = ? OR RECEIVER = ?) AND ISBROADCAST = false\
                 ORDER BY timestamp ASC\
                 LIMIT ?\
-                OFFSET ?';
+                OFFSET ?";
 
     return new Promise((resolve, reject) => {
-      const messages : Payload[] = [];
-      this.db.each(sql, [uuid, uuid, to-from, from], (err, row) => {
+      const messages: Payload[] = [];
+      this.db.each(sql, [uuid, uuid, to - from, from], (err, row) => {
         if (err) {
           reject("sql failed: " + sql);
         } else {
           messages.push(PayloadUtils.jsonToPayload(
             {uuid: row.sender,
-            type: 'message',
+            type: "message",
             timestamp: row.timestamp,
             nickname: row.nickname,
             message: row.message}));
@@ -119,16 +111,135 @@ export class DataService {
         } else {
           resolve (messages);
         }
-      })
+      });
     });
   }
 
   /* Store message in chat history associated with the provided UUID */
-  public storeMessage(payload: Payload, sender: string, receiver: string): void {
-  	this.db.run(`INSERT INTO messages(sender, receiver, isBroadcast, nickname, timestamp, message)
-  					VALUES(?, ?, ?, ?, ?, ?)`,
-  					[sender, receiver, payload.type==='broadcast', payload.nickname, payload.timestamp, payload.message]);
+  public storeMessage(p: Payload, sender: string, receiver: string): void {
+    const sql = `INSERT INTO messages(sender, receiver, isBroadcast, nickname, timestamp, message) \
+                 VALUES(?, ?, ?, ?, ?, ?)`;
+    this.db.run(sql , [sender, receiver, p.type === "broadcast", p.nickname, p.timestamp, p.message]);
   }
 
+  // adds a user to the database
 
+  public addUser(u: User) {
+    const sql = `INSERT INTO users(uuid, ip, lastHeartbeat, nickname, isOnline, isBlocked) \
+               VALUES(?, ?, ?, ?, ?, ?)`;
+    this.db.run(sql , [u.uuid, u.ip, u.lastHeartbeat, u.nickname, true, false]);
+  }
+
+  public getUser(uuid: string): Promise<User> {
+    const sql = "SELECT * FROM users WHERE uuid = ?";
+    return new Promise<User>((resolve, reject) => {
+      this.db.get(sql, [uuid], (err, row) => {
+        if (err) {
+          reject("sql failed: " + sql);
+        } else {
+          if (row === undefined) {
+            resolve(null);
+          } else {
+            const user: User = {
+              uuid: row.uuid,
+              nickname: row.nickname,
+              isBlocked: row.isBlocked,
+              ip: row.ip,
+              lastHeartbeat: row.lastHeartbeat,
+              isOnline: row.isOnline,
+            };
+            resolve(user);
+          }
+        }
+      });
+    });
+  }
+
+  public getUserIP(uuid: string): Promise<string> {
+    const sql = "SELECT * FROM users WHERE uuid = ? AND isOnline = ?";
+    return new Promise<string>((resolve, reject) => {
+      this.db.get(sql, [uuid, true], (err, row) => {
+        if (err) {
+          reject("sql failed: " + sql);
+        } else {
+          if (row === undefined) {
+            resolve(null);
+          } else {
+            resolve(row.ip);
+          }
+        }
+      });
+    });
+  }
+
+  public getOfflineUsers(): Promise<User[]> {
+    const sql = "SELECT * FROM users WHERE isOnline = ?";
+    const users: User[] = [];
+    return new Promise<User[]>((resolve, reject) => {
+      this.db.each(sql, [false], (err, row) => {
+        if (err) {
+          reject("sql failed: " + sql);
+        } else {
+          const u: User = {
+            uuid: row.uuid,
+            nickname: row.nickname,
+            isBlocked: row.isBlocked,
+            ip: row.ip,
+            lastHeartbeat: row.lastHeartbeat,
+            isOnline: row.isOnline,
+          };
+          users.push(u);
+        }
+      }, (err, n) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(users);
+        }
+      });
+    });
+  }
+
+  public getNonBlockedOnlineUsers(): Promise<User[]> {
+    const sql = "SELECT * FROM users WHERE isBlocked = ? AND isOnline = ?";
+    const users: User[] = [];
+    return new Promise<User[]>((resolve, reject) => {
+      this.db.each(sql, [false, true], (err, row) => {
+        if (err) {
+          reject("sql failed: " + sql);
+        } else {
+          const u: User = {
+            uuid: row.uuid,
+            nickname: row.nickname,
+            isBlocked: row.isBlocked,
+            ip: row.ip,
+            lastHeartbeat: row.lastHeartbeat,
+            isOnline: row.isOnline,
+          };
+          users.push(u);
+        }
+      }, (err, n) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(users);
+        }
+      });
+    });
+  }
+
+  public updateUserHeartbeat(uuid: string, nickname: string, address: string, timeStamp: Date) {
+    const sql = "UPDATE users SET nickname = ?, ip = ?, lastHeartbeat = ?, isOnline = ? WHERE uuid = ?";
+    this.db.run(sql, [nickname, address, timeStamp, true, uuid]);
+  }
+
+  public updateUserNickname(uuid: string, newNickname: string) {
+    const sql = "UPDATE users SET nickname = ? WHERE uuid = ?";
+    this.db.run(sql, [newNickname, uuid]);
+  }
+
+  public timeoutOfflineUsers() {
+    const sql = "UPDATE users SET isOnline = ? WHERE isOnline = ? AND ? - lastHeartbeat > ?";
+    this.db.run(sql, [false, true, Settings.ONLINE_USER_TIMEOUT]);
+  }
 }
